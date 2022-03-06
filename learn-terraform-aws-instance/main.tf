cf89e3a7cc4995ef0754ea059d5747704edff008
ianbrdeguzman <ianbrdeguzman@gmail.com>
2022-03-06 11:48:04 -0500

    added s3 remote backend

--- learn-terraform-aws-instance/main.tf
+++ learn-terraform-aws-instance/main.tf
@@ -7,6 +7,14 @@ terraform {
   }
 
   required_version = ">= 0.14.9"
+
+  backend "s3" {
+    bucket         = "learn-terraform-state-remote-aws-ian"
+    key            = "terraform/state/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "learn-terraform-state-remote-locking-ian"
+    encrypt        = true
+  }
 }
 
 provider "aws" {
@@ -20,5 +28,36 @@ resource "aws_instance" "app_server" {
 
   tags = {
     Name = var.instance_name
+  }
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "learn-terraform-state-remote-aws-ian"
+
+  lifecycle {
+    prevent_destroy = false // should be true on live
+  }
+
+  versioning {
+    enabled = true
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
+
+resource "aws_dynamodb_table" "terraform_state" {
+  name         = "learn-terraform-state-remote-locking-ian"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
   }
 }
